17dbcdfb171c788c0b8355aa2986ba2f56d961f1
Natalie Taktachev <ntaktach@gmail.com>
2019-07-17 19:01:56 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,10 +2,11 @@
 
 def countdown(number)
 number = integer
+
   while number > 0
-    puts "#{number - 1} SECOND(S)!"
+    puts "#{number} SECOND(S)!"
     number -= 1
-  until number == 0
+
     puts "HAPPY NEW YEAR!"
   end
 
