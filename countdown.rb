db2b748cd0f818631e877c759c23c63b8e50d493
Natalie Taktachev <ntaktach@gmail.com>
2019-07-17 18:48:26 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,13 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  number = 0
+  
+  counter = 0
+  while number = > 0 
+  number = number - 1 
+  counter -= 1
+  end
+when counter == 0
+  puts "HAPPY NEW YEAR!"
 end
